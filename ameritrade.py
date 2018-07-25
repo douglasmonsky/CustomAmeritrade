7f13a97243f3f6b90f5ea53f0e3c5b46d1b7ac41
Douglas Monsky <douglas.monsky@gmail.com>
2018-07-25 14:24:11 -0400

    adjust headers

--- ameritrade.py
+++ ameritrade.py
@@ -26,27 +26,31 @@ class Ameritrade:
             with open(f'auth_token_{self.account_id}.pickle', 'rb') as f:
                 data = pickle.load(f)
                 self.auth_token, self.expire_time = data
+        self.default_headers = {'Content-Type': 'application/json;charset=UTF-8', 'Authorization': f'Bearer {self.auth_token}'}
      
     def check_expiration(self):
         if time.time() > self.expire_time:
             self.refresh_auth()
+
+    def get_account_positions(self, info_type='positions'):
+        self.check_expiration()
+        data = requests.get(f"https://api.tdameritrade.com/v1/accounts/{self.account_id}?fields={info_type}", headers=self.default_headers)
+        return data.json()
     
     def get_live_quote(self, symbol):
         self.check_expiration()  
-        headers = {'Content-Type': 'application/json;charset=UTF-8', 'Authorization': f'Bearer {self.auth_token}'}
-        live_quote = requests.get(f"https://api.tdameritrade.com/v1/marketdata/{symbol}/quotes?apikey={client_id}", headers=headers)
+        live_quote = requests.get(f"https://api.tdameritrade.com/v1/marketdata/{symbol}/quotes?apikey={self.client_id}", headers=self.default_headers)
         return live_quote.json()
 
     def get_transactions(self, start_date, end_date, data_type='ALL'):
         self.check_expiration()
-        headers = {'Content-Type': 'application/json;charset=UTF-8', 'Authorization': f'Bearer {self.auth_token}'}
-        transactions = requests.get(f"https://api.tdameritrade.com/v1/accounts/{self.account_id}/transactions?type={data_type}&startDate={start_date}&endDate={end_date}", headers=headers)
+        transactions = requests.get(f"https://api.tdameritrade.com/v1/accounts/{self.account_id}/transactions?type={data_type}&startDate={start_date}&endDate={end_date}", headers=self.default_headers)
         return transactions.json()
 
     def refresh_auth(self):
         print('refreshing token')
         headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
-        data = { 'grant_type': 'refresh_token', 'refresh_token': self.refresh_token, 'client_id': }
+        data = { 'grant_type': 'refresh_token', 'refresh_token': self.refresh_token, 'client_id': self.client_id}
         authReply = requests.post('https://api.tdameritrade.com/v1/oauth2/token', headers=headers, data=data)
         self.auth_token = authReply.json()['access_token']
         self.expire_time = time.time() + 1740 #29 mins, so token refreshes 1 min before expiration.
@@ -60,17 +64,18 @@ class Ameritrade:
     #     data = {'periodType': period_type, 'frequencyType': frequency_type, 'period': period, 'frequency': frequency,
     #             'endDate': end_date, 'startDate:': start_date, 'needExtendedHoursData': extended_hours}
     #     headers = {'Content-Type': 'application/json;charset=UTF-8', 'Authorization': f'Bearer {self.auth_token}'}
-    #     price_history = requests.get(f"https://api.tdameritrade.com/v1/marketdata/{symbol}/pricehistory?apikey={client_id}", headers=headers, data=data)
+    #     price_history = requests.get(f"https://api.tdameritrade.com/v1/marketdata/{symbol}/pricehistory?apikey={self.client_id}", headers=headers, data=data)
     #     return price_history.json()
 
 
 if __name__ == '__main__':
-    from privateinfo import MainAccount
-    ameritrade = Ameritrade(MainAccount)
-    transactions = ameritrade.get_transactions('2018-01-01', '2018-07-23')
-    all_fees = 0
-    for transaction in transactions:
-        fees = transaction['fees']
-        total_fee = sum(fees.values())
-        all_fees += total_fee
-    print(all_fees)
+    from privateinfo import MainAccount, client_id
+    ameritrade = Ameritrade(MainAccount, client_id)
+    # transactions = ameritrade.get_transactions('2018-01-01', '2018-07-23')
+    # all_fees = 0
+    # for transaction in transactions:
+    #     fees = transaction['fees']
+    #     total_fee = sum(fees.values())
+    #     all_fees += total_fee
+    # print(all_fees)
+    print(ameritrade.get_account_positions()['securitiesAccount']['currentBalances']['liquidationValue'])
